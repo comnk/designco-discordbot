2f032f75fa7470af4d98dcf705e80bae3ae28eb0
comnk <vhsiao36@gmail.com>
2024-08-23 14:56:57 -0700

    can attach image to scheduled event

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const dotenv = require('dotenv');
+const axios = require('axios');
+const prompt=require("prompt-sync")({sigint:true});
 
 dotenv.config();
 
@@ -14,9 +16,13 @@ const client = new Client({
 
 console.log(token);
 
-async function scheduleEvent(name, startTime, endTime, description) {
+async function scheduleEvent(name, startTime, endTime, description, imageUrl) {
     try {
         const guild = await client.guilds.fetch(process.env.GUILD_ID);
+
+        const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(response.data, 'binary');
+        const imageBase64 = `data:image/jpeg;base64,${imageBuffer.toString('base64')}`;
 
         const scheduledEvent = await guild.scheduledEvents.create({
             name: name,
@@ -25,7 +31,8 @@ async function scheduleEvent(name, startTime, endTime, description) {
             scheduledEndTime: endTime,
             entityType: 3,
             entityMetadata: { location: "TEST"},
-            description: description
+            description: description,
+            image: imageBase64
         });
 
         const channel = await client.channels.fetch(process.env.CHANNEL_ID);
@@ -41,12 +48,15 @@ async function scheduleEvent(name, startTime, endTime, description) {
 client.once(Events.ClientReady, readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 
-    const name = "Community Event";
-    const description = "Urgent Community Event";
+    let name = prompt("Name of event: ");
+    let description = prompt("Provide description of event: ");
     const startTime = new Date('2024-08-26T15:00:00Z');
     const endTime = new Date('2024-08-26T16:00:00Z');
 
-    scheduleEvent(name, startTime, endTime, description);
+    // test image
+    const imageUrl = "https://th.bing.com/th/id/R.b278428a21699d82d8bda5ecff4d3fba?rik=VCLWUCs8MAqrJw&pid=ImgRaw&r=0";
+
+    scheduleEvent(name, startTime, endTime, description, imageUrl);
 });
 
 client.login(token);
